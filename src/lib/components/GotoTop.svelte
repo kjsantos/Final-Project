cfa4652786c7d24d933e3d6b8a2a1d865a4e9e52
Kailen <kailenjsantos@gmail.com>
2023-08-15 18:33:22 -0400

    Staged changes for functionality

---
+++ src/lib/components/GotoTop.svelte
@@ -0,0 +1,63 @@
+<script>
+	import { fade, fly } from 'svelte/transition'
+
+	export let showAtPixel = 2000
+
+	let scrollHeight = 0
+
+	const gotoTop = () => {
+		window.scrollTo({ top: 0, behavior: 'smooth' })
+	}
+
+	$: showGotoTop = scrollHeight > showAtPixel
+</script>
+
+{#if showGotoTop}
+	<button
+		on:click={gotoTop}
+		class="goto__top"
+		title="Go to top"
+		in:fly={{ y: 300, duration: 100 }}
+		out:fade={{ duration: 100 }}>
+		<div class="container flex space-x-1">
+			<p class="align-top" style="color: white;">Go To Top</p>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="32"
+				height="32"
+				viewBox="0 -4 28 28">
+				<g fill="none" stroke="currentColor" stroke-width="1.5">
+					<path
+						fill="white"
+						d="M2 12c0-4.714 0-7.071 1.464-8.536C4.93 2 7.286 2 12 2c4.714 0 7.071 0 8.535 1.464C22 4.93 22 7.286 22 12c0 4.714 0 7.071-1.465 8.535C19.072 22 16.714 22 12 22s-7.071 0-8.536-1.465C2 19.072 2 16.714 2 12Z" />
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="m15 13.5l-3-3l-3 3" />
+				</g>
+			</svg>
+		</div>
+	</button>
+{/if}
+
+<svelte:window bind:scrollY={scrollHeight} />
+
+<style>
+	.goto__top {
+		position: fixed;
+		right: 1rem;
+		bottom: 1rem;
+		flex: content;
+		z-index: 999;
+		transition-duration: 300ms;
+		cursor: pointer;
+		border: none;
+		border-radius: 0.5rem;
+		background: none;
+		padding: 0;
+		color: #999;
+	}
+	.goto__top:hover {
+		transform: translateY(-10px);
+	}
+</style>
---
+++ src/lib/components/Projects.svelte
@@ -0,0 +1,380 @@
+<script>
+	import Saos from 'saos'
+</script>
+
+<main>
+	<section>
+		<Saos
+			animation={'fade-in 1.2s cubic-bezier(0.390, 0.575, 0.565, 1.000) both'}
+			once={true}>
+			<h2 class="text-center" style="color: white">
+				Here is where Projects will go
+			</h2>
+		</Saos>
+		<Saos
+			animation={'fade-in 1.2s cubic-bezier(0.390, 0.575, 0.565, 1.000) both'}
+			animation_out={'slide-out-fwd-center 0.7s cubic-bezier(0.550, 0.085, 0.680, 0.530) both'}
+			top={250}
+			bottom={250}>
+			<div
+				class="mx-52 my-10 flex h-full justify-between rounded-t-3xl rounded-bl-3xl p-8 shadow-2xl shadow-slate-800"
+				style="background-color: #FFF5F5">
+				<div class="justify-items-left container w-auto">
+					<div class="pl-26 w-3/5">
+						<h2>Interactive Campaign Finance Story</h2>
+						<br />
+						<p>
+							A click-through visualization created using Svelte
+							and Tailwind.css. Data obtained from OCPF.
+						</p>
+					</div>
+				</div>
+				<img
+					class="justify-items-left h-1/4 w-1/4 py-12"
+					src="images/Screenshot 2023-03-20 at 5.50.04 PM.png"
+					alt="" />
+			</div>
+		</Saos>
+		<Saos
+			animation={'fade-in 1.2s cubic-bezier(0.390, 0.575, 0.565, 1.000) both'}
+			animation_out={'slide-out-fwd-center 0.7s cubic-bezier(0.550, 0.085, 0.680, 0.530) both'}
+			top={250}
+			bottom={250}>
+			<div
+				class="mx-52 my-10 flex h-full justify-between rounded-t-3xl rounded-bl-3xl p-8 shadow-2xl shadow-slate-800"
+				style="background-color: #FFF5F5">
+				<div class="justify-items-left container w-auto">
+					<div class="pl-26 w-2/5">
+						<h2>NYPD Complaint Review Analysis</h2>
+						<br />
+						<p>
+							Mini investigative piece featuring D3 visualizations
+							and analysis in Python and R. Created using
+							Reveal.js.
+						</p>
+					</div>
+				</div>
+			</div>
+		</Saos>
+		<Saos
+			animation={'fade-in 1.2s cubic-bezier(0.390, 0.575, 0.565, 1.000) both'}
+			animation_out={'slide-out-fwd-center 0.7s cubic-bezier(0.550, 0.085, 0.680, 0.530) both'}
+			top={250}
+			bottom={250}>
+			<div
+				class="mx-52 my-10 flex h-full justify-between rounded-t-3xl rounded-bl-3xl p-8 shadow-2xl shadow-slate-800"
+				style="background-color: #FFF5F5">
+				<div class="justify-items-left container w-auto">
+					<div class="pl-26 w-3/5">
+						<h2>Interactive Campaign Finance Story</h2>
+						<br />
+						<p>
+							A click-through visualization created using Svelte
+							and Tailwind.css. Data obtained from OCPF.
+						</p>
+					</div>
+				</div>
+				<img
+					class="justify-items-left h-1/4 w-1/4 py-12"
+					src="images/Screenshot 2023-03-20 at 5.50.04 PM.png"
+					alt="" />
+			</div>
+		</Saos>
+		<Saos
+			animation={'fade-in 1.2s cubic-bezier(0.390, 0.575, 0.565, 1.000) both'}
+			animation_out={'slide-out-fwd-center 0.7s cubic-bezier(0.550, 0.085, 0.680, 0.530) both'}
+			top={250}
+			bottom={250}>
+			<div
+				class="mx-52 my-10 flex h-full justify-between rounded-t-3xl rounded-bl-3xl p-8 shadow-2xl shadow-slate-800"
+				style="background-color: #FFF5F5">
+				<div class="justify-items-left container w-auto">
+					<div class="pl-26 w-2/5">
+						<h2>NYPD Complaint Review Analysis</h2>
+						<br />
+						<p>
+							Mini investigative piece featuring D3 visualizations
+							and analysis in Python and R. Created using
+							Reveal.js.
+						</p>
+					</div>
+				</div>
+			</div>
+		</Saos>
+		<Saos
+			animation={'fade-in 1.2s cubic-bezier(0.390, 0.575, 0.565, 1.000) both'}
+			animation_out={'slide-out-fwd-center 0.7s cubic-bezier(0.550, 0.085, 0.680, 0.530) both'}
+			top={250}
+			bottom={250}>
+			<div
+				class="mx-52 my-10 flex h-full justify-between rounded-t-3xl rounded-bl-3xl p-8 shadow-2xl shadow-slate-800"
+				style="background-color: #FFF5F5">
+				<div class="justify-items-left container w-auto">
+					<div class="pl-26 w-3/5">
+						<h2>Interactive Campaign Finance Story</h2>
+						<br />
+						<p>
+							A click-through visualization created using Svelte
+							and Tailwind.css. Data obtained from OCPF.
+						</p>
+					</div>
+				</div>
+			</div>
+		</Saos>
+		<Saos
+			animation={'fade-in 1.2s cubic-bezier(0.390, 0.575, 0.565, 1.000) both'}
+			animation_out={'slide-out-fwd-center 0.7s cubic-bezier(0.550, 0.085, 0.680, 0.530) both'}
+			top={250}
+			bottom={250}>
+			<div
+				class="mx-52 my-10 flex h-full justify-between rounded-t-3xl rounded-bl-3xl p-8 shadow-2xl shadow-slate-800"
+				style="background-color: #FFF5F5">
+				<div class="justify-items-left container w-auto">
+					<div class="pl-26 w-2/5">
+						<h2>NYPD Complaint Review Analysis</h2>
+						<br />
+						<p>
+							Mini investigative piece featuring D3 visualizations
+							and analysis in Python and R. Created using
+							Reveal.js.
+						</p>
+					</div>
+				</div>
+			</div>
+		</Saos>
+	</section>
+</main>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: row;
+		flex-wrap: wrap;
+		justify-content: space-around;
+		margin: 0 auto;
+		overflow-x: visible;
+	}
+
+	div > p:nth-child(2) {
+		text-align: start;
+	}
+
+	@keyframes -global-scale-in-center {
+		0% {
+			transform: scale(0);
+			opacity: 1;
+		}
+		100% {
+			transform: scale(1);
+			opacity: 1;
+		}
+	}
+
+	@keyframes -global-rotate-in-center {
+		0% {
+			transform: rotate(-360deg);
+			opacity: 0;
+		}
+		100% {
+			transform: rotate(0);
+			opacity: 1;
+		}
+	}
+
+	@keyframes -global-slide-in-top {
+		0% {
+			transform: translateY(-1000px);
+			opacity: 0;
+		}
+		100% {
+			transform: translateY(0);
+			opacity: 1;
+		}
+	}
+
+	@keyframes -global-slide-in-fwd-tr {
+		0% {
+			transform: translateZ(-1200px) translateY(-700px) translateX(500px);
+			opacity: 0;
+		}
+		100% {
+			transform: translateZ(0) translateY(0) translateX(0);
+			opacity: 1;
+		}
+	}
+
+	@keyframes -global-slide-in-elliptic-top-fwd {
+		0% {
+			transform: translateY(-600px) rotateX(-30deg) scale(0);
+			transform-origin: 50% 100%;
+			opacity: 0;
+		}
+		100% {
+			transform: translateY(0) rotateX(0) scale(1);
+			transform-origin: 50% 1400px;
+			opacity: 1;
+		}
+	}
+
+	@keyframes -global-roll-in-left {
+		0% {
+			transform: translateX(-800px) rotate(-540deg);
+			opacity: 0;
+		}
+		100% {
+			transform: translateX(0) rotate(0deg);
+			opacity: 1;
+		}
+	}
+
+	@keyframes -global-roll-in-blurred-left {
+		0% {
+			transform: translateX(-1000px) rotate(-720deg);
+			opacity: 0;
+			filter: blur(50px);
+		}
+		100% {
+			transform: translateX(0) rotate(0deg);
+			opacity: 1;
+			filter: blur(0);
+		}
+	}
+
+	@keyframes -global-tilt-in-fwd-tr {
+		0% {
+			transform: rotateY(20deg) rotateX(35deg) translate(300px, -300px)
+				skew(-35deg, 10deg);
+			opacity: 0;
+		}
+		100% {
+			transform: rotateY(0) rotateX(0deg) translate(0, 0) skew(0deg, 0deg);
+			opacity: 1;
+		}
+	}
+
+	@keyframes -global-swing-in-top-fwd {
+		0% {
+			transform: rotateX(-100deg);
+			transform-origin: top;
+			opacity: 0;
+		}
+		100% {
+			transform: rotateX(0deg);
+			transform-origin: top;
+			opacity: 1;
+		}
+	}
+
+	@keyframes -global-puff-in-center {
+		0% {
+			transform: scale(2);
+			opacity: 0;
+			filter: blur(4px);
+		}
+		100% {
+			transform: scale(1);
+			opacity: 1;
+			filter: blur(0px);
+		}
+	}
+
+	@keyframes -global-fade-in {
+		0% {
+			opacity: 0;
+		}
+		100% {
+			opacity: 1;
+		}
+	}
+
+	/* EXITS */
+	@keyframes -global-scale-out-center {
+		0% {
+			transform: scale(1);
+			opacity: 1;
+		}
+		100% {
+			transform: scale(0);
+			opacity: 1;
+		}
+	}
+
+	@keyframes -global-rotate-out-center {
+		0% {
+			transform: rotate(0);
+			opacity: 1;
+		}
+		100% {
+			transform: rotate(-360deg);
+			opacity: 0;
+		}
+	}
+
+	@keyframes -global-rotate-out-2-cw {
+		0% {
+			transform: rotate(0);
+			opacity: 1;
+		}
+		100% {
+			transform: rotate(45deg);
+			opacity: 0;
+		}
+	}
+
+	@keyframes -global-swirl-out-bck {
+		0% {
+			transform: rotate(0) scale(1);
+			opacity: 1;
+		}
+		100% {
+			transform: rotate(-540deg) scale(0);
+			opacity: 0;
+		}
+	}
+
+	@keyframes -global-flip-out-hor-top {
+		0% {
+			transform: rotateX(0);
+			opacity: 1;
+		}
+		100% {
+			transform: rotateX(70deg);
+			opacity: 0;
+		}
+	}
+
+	@keyframes -global-slide-out-top {
+		0% {
+			transform: translateY(0);
+			opacity: 1;
+		}
+		100% {
+			transform: translateY(-1000px);
+			opacity: 0;
+		}
+	}
+
+	@keyframes -global-slide-out-fwd-center {
+		0% {
+			transform: translateZ(1);
+			opacity: 1;
+		}
+		100% {
+			transform: translateZ(600px);
+			opacity: 0;
+		}
+	}
+
+	@keyframes -global-slide-out-elliptic-top-bck {
+		0% {
+			transform: translateY(0) rotateX(0) scale(1);
+			transform-origin: 50% 1400px;
+			opacity: 1;
+		}
+		100% {
+			transform: translateY(-600px) rotateX(-30deg) scale(0);
+			transform-origin: 50% 100%;
+			opacity: 1;
+		}
+	}
+</style>
