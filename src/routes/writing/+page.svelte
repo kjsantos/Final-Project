ac2077af105acbc2124a1fc99ddcc53dd45172e6
Kailen <kailenjsantos@gmail.com>
2023-08-23 15:33:51 -0400

    Fixed routing issue

--- src/routes/writing/+page.svelte
+++ src/routes/writing/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import GotoTop from '$lib/components/GotoTop.svelte'
 	import Writing from '$lib/components/Writing.svelte'
+
+	export let data
 </script>
 
 <main>
--- src/routes/writing/[slug]/+layout.svelte
+++ src/routes/writing/[slug]/+layout.svelte
@@ -9,19 +9,4 @@
 </div>
 
 <style>
-	p {
-		color: white;
-		font-style: normal;
-		font-weight: 600;
-		font-size: 1rem;
-		font-family: 'josefin-sans', sans-serif;
-	}
-
-	b {
-		color: slategrey;
-		font-weight: 800;
-		font-size: 1rem;
-		line-height: 2rem;
-		font-family: 'josefin-sans', sans-serif;
-	}
 </style>
